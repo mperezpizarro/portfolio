f98c76646c03626a538d12933a1f83c4305e8333
mperezpizarro <marioperezpizarro@gmail.com>
2022-12-09 18:37:22 +0100

    Improved Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import './Navbar.css'
 import logo from '/logo_h_color.png'
 import { Menu, X } from 'react-feather'
 
-const Navbar = () => {  
+const Navbar = () => {
     const menuRef = useRef()
 
     const showMenu = () => {
@@ -13,25 +13,27 @@ const Navbar = () => {
     const hideMenu = () => {
         menuRef.current.classList.remove('showed')
     }
-    
+
     return (
         <div className="navbar-container">
-            <div className="logo"><a href="#"><img src={logo} alt="Logo personal" /></a></div>
-            <nav>
-                <ul>
-                    <li><a href="#experiencia">experiencia</a></li>
-                    <li><a href="#proyectos">proyectos</a></li>
-                    <li><a href="#tecnologías">tecnologías</a></li>
-                </ul>
-            </nav>
-            <div className="mobile-menu">
-                <button onClick={showMenu}><Menu /></button>
-                <ul className='mobile-links' ref={menuRef}>
-                    <button onClick={hideMenu}><X /></button>
-                    <li><a href="#experiencia" onClick={hideMenu}>experiencia</a></li>
-                    <li><a href="#proyectos" onClick={hideMenu}>proyectos</a></li>
-                    <li><a href="#tecnologías" onClick={hideMenu}>tecnologías</a></li>
-                </ul>
+            <div className='navbar-wrapper'>
+                <div className="logo"><a href="#"><img src={logo} alt="Logo personal" /></a></div>
+                <nav>
+                    <ul>
+                        <li><a href="#experiencia">experiencia</a></li>
+                        <li><a href="#proyectos">proyectos</a></li>
+                        <li><a href="#tecnologías">tecnologías</a></li>
+                    </ul>
+                </nav>
+                <div className="mobile-menu">
+                    <button onClick={showMenu}><Menu /></button>
+                    <ul className='mobile-links' ref={menuRef}>
+                        <button onClick={hideMenu}><X /></button>
+                        <li><a href="#experiencia" onClick={hideMenu}>experiencia</a></li>
+                        <li><a href="#proyectos" onClick={hideMenu}>proyectos</a></li>
+                        <li><a href="#tecnologías" onClick={hideMenu}>tecnologías</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     )
