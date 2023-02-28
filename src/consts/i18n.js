71c72a4320a6d0c5fbe00375ce995dba7a85f268
mperezpizarro <marioperezpizarro@gmail.com>
2023-02-28 13:19:31 +0100

    Translated projects section

--- src/consts/i18n.js
+++ src/consts/i18n.js
@@ -36,10 +36,20 @@ export const i18nExperience = {
 
 export const i18nProjects = {
     es: {
-        title: "MIS PROYECTOS"
+        title: "MIS PROYECTOS",
+        badajoz_unida: "Proyecto en grupo de fin de curso que consistía en el prototipo de una plataforma para organizar y unirse a diferentes eventos multiculturales en la ciudad de Badajoz.",
+        password_generator: "Aplicación sencilla para generar una contraseña aleatoria. Permite ajustar la longitud de la misma de 8 a 24 caracteres.",
+        animal_pics: "Aplicación que muestra una foto al azar de un Shiba, pato o zorro gracias a la ayuda de distintas APIs.",
+        blog: "Blog personal donde subo artículos relacionados con el diseño y desarrollo frontend.",
+        fichas_doblaje: "Proyecto para aprender Nuxt que busca y muestra datos a través de una API creada en el mismo framework."
     },
     en: {
-        title: "MY PROJECTS"
+        title: "MY PROJECTS",
+        badajoz_unida: "Group class project consisting of an app prototype to organize and join to different multicultural events in Badajoz's city.",
+        password_generator: "Simple app to generate random passwords. Allows you to adjust the length between 8 to 24 characters.",
+        animal_pics: "App that shows random pics of shibas, ducks or foxes using different APIs.",
+        blog: "Personal blog where I write frontend development and design related articles.",
+        fichas_doblaje: "Project for learning Nuxt which searches and shows data through an API created in the framework itself."
     }
 }
 
